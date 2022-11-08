fefa1493e7af21338a6b042cb6e03f4b4dd2f0a1
Dry-Audrey <audreydev.br@gmail.com>
2022-11-08 09:29:54 -0300

    Adicionando Login :monkey:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './Components/estaticos/navbar/Navbar';
+import Footer from './Components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Sobre from './paginas/sobre/Sobre';
+import Login from './paginas/login/Login'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path="/home" element={<Home />}/>
+            <Route path="/sobre" element={<Sobre />}/>
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
 export default App;
