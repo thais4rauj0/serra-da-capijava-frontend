9febd59fb4987d03496b1aca80f6155443404cc3
nathaliebfm <natibfm@hotmail.com>
2022-11-08 15:50:14 -0300

    Inclusão da Navbar ao projeto :monkey:

--- src/Components/estaticos/footer/Footer.tsx
+++ src/Components/estaticos/footer/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
                 <Grid alignItems="center" item xs={12}>
                     <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="body1" align="center" gutterBottom className="textos">Nos acompanhe no Instagram:</Typography>
+                            <Typography variant="body1" align="center" gutterBottom className="textoRedes">Nos acompanhe no Instagram:</Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.instagram.com/capijavageneration/" target="_blank">
