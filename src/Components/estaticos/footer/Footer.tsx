d6ee2739bf51c13298f036833790ab14d495c5c8
carolmatosss <carolyne_nascimento@hotmail.com>
2022-11-19 20:01:55 -0300

    Arrumando footer

--- src/Components/estaticos/footer/Footer.tsx
+++ src/Components/estaticos/footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import InstagramIcon from '@material-ui/icons/Instagram';
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import "./Footer.css"
 import { useSelector } from "react-redux";
 import { TokenState } from "../../../store/tokens/TokenReduce";
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 function Footer() {
     const token = useSelector<TokenState,TokenState["tokens"]>((state)=> state.tokens);
     
@@ -14,17 +15,30 @@ function Footer() {
       footerComponent =
       <Grid container direction="row" justifyContent="center" alignItems="center">
       <Grid alignItems="center" item xs={12}>
-          <Box className="box1">
-              <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                  <Typography variant="body1" align="center" gutterBottom className="textos">Nos acompanhe no Instagram:</Typography>
-              </Box>
-              <Box display="flex" alignItems="center" justifyContent="center">
+          <Box className="box1" display= "flex" justifyContent="space-evenly">
+          <Box display="flex" alignItems="center" flexDirection="column" justifyContent="space-evenly">
+            <Typography variant="body1" align="center" gutterBottom className="textos">Nossas redes:</Typography>
+            <Box display="flex" alignItems="center"  justifyContent="space-evenly">
                   <a href="https://www.instagram.com/capijavageneration/" target="_blank">
                       <InstagramIcon className="redes"/>
                   </a>
+                  <a href="https://github.com/thais4rauj0/serra-da-capijava-frontend" target="_blank">
+                      <GitHubIcon className="redes"/>
+                  </a>
+                  </Box>
               </Box>
-          </Box>
-          <Box className="box2">
+              <Divider orientation="vertical" textAlign="center" flexItem />
+              <Box display="flex" alignItems="center" flexDirection="column" justifyContent="space-evenly">
+            <Typography variant="body1" align="center" gutterBottom className="textos">Nossos contato:</Typography>
+            <Box display="flex" alignItems="center"  justifyContent="space-evenly">
+            <EmailIcon className="redes"/>
+            <Typography variant="body1" align="center" gutterBottom className="textos"> generationgrupo2@gmail.com</Typography>
+            
+                  </Box>
+              </Box>
+              </Box>
+             
+              <Box className="box2">
               <Box paddingTop={1}  display="flex" justifyContent="center" alignItems="center">
                   <Typography variant="subtitle2" align="center" gutterBottom className="textos" >© 2022 Copyright:</Typography>
               </Box>
@@ -34,6 +48,7 @@ function Footer() {
           </Box>
       </Grid>
   </Grid>
+    
     }
     
     return (
