f69dd9f18c8326e5c31eaf4f7015da5b7778d09f
Audrey-Albuquerque <audreydev.br@gmail.com>
2022-11-19 18:01:51 -0300

    adiconando banner

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -185,19 +185,10 @@ function Home() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo">Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="subtitulo">Expresse aqui os seus pensamentos e opiniões!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button variant="outlined" className="botao">Ver Postagens</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <img src="https://i.imgur.com/clJVUH7.png" alt="" width="500px" height="500px" />
+               
+                <Grid item className="banner" xs={12} >
+                    
+                    
                 </Grid>
                 <Grid xs={12} className="produtos">
                 </Grid>
